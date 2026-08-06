9b574df48222b5b2090f0b921c99747f35e73130
Sisi Wu <sw223hk@student.lnu.se>
2026-08-07 00:43:16 +0200

    create empty game board

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,12 @@
+import '../App.css'
+
+export default function GameBoard(){
+    return (
+        <div>
+            <h2>Game board</h2>
+            <div className="wrapper">
+            </div>
+
+        </div>
+    )
+}
